63b533c9e0c08725ccf4fceb4fd245416b4d9201
Scott Schermerhorn <plaidscott@gmail.com>
2017-07-14 08:50:19 -0600

    adds MainNavBar component with basic setup/links/styling

---
+++ src/Components/MainNavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Nav, NavItem, NavLink, Navbar, NavbarBrand, Collapse } from 'reactstrap';
+
+
+class MainNavBar extends Component {
+    render() {
+        return (
+            <div className="MainNavBar">
+                <Navbar color="faded" light toggleable>
+                    <NavbarBrand href="#">TruePrice</NavbarBrand>
+                    <Collapse isOpen={true} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">First Link</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Second Link</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default MainNavBar;
